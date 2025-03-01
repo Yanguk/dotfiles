25b69f965f018284d91c08ad07bfa01dc9ad7c89
Yanguk <yanguk@duck.com>
2025-03-01 17:13:03 +0900

    nix 주석 정리 및 home.nix 삭제

--- nix/flake.nix
+++ nix/flake.nix
@@ -6,8 +6,6 @@
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-    # home-manager.url = "github:nix-community/home-manager";
-    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -16,20 +14,26 @@
       nix-darwin,
       nixpkgs,
       nix-homebrew,
-    # home-manager,
     }:
     let
       username = "yanguk";
       configuration =
         { pkgs, ... }:
         {
-          nix.enable = false;
-          nixpkgs.config = {
-            allowUnfree = true;
+          nix = {
+            enable = false;
+            settings = {
+              experimental-features = "nix-command flakes";
+            };
           };
 
-          # List packages installed in system profile. To search by name, run:
-          # $ nix-env -qaP | grep wget
+          nixpkgs = {
+            config = {
+              allowUnfree = true;
+            };
+            hostPlatform = "aarch64-darwin";
+          };
+
           environment.systemPackages = with pkgs; [
             neovim
             tmux
@@ -89,15 +93,6 @@
             onActivation.upgrade = true;
           };
 
-          # Necessary for using flakes on this system.
-          nix.settings.experimental-features = "nix-command flakes";
-
-          # Enable alternative shell support in nix-darwin.
-          # programs.fish.enable = true;
-
-          # The platform the configuration will be used on.
-          nixpkgs.hostPlatform = "aarch64-darwin";
-
           system = {
             # Set Git commit hash for darwin-version.
             configurationRevision = self.rev or self.dirtyRev or null;
@@ -147,26 +142,11 @@
           nix-homebrew.darwinModules.nix-homebrew
           {
             nix-homebrew = {
-              # Install Homebrew under the default prefix
               enable = true;
-
-              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-              # enableRosetta = true;
-
               # User owning the Homebrew prefix
               user = username;
             };
           }
-          # home-manager.darwinModules.home-manager
-          # {
-          #   users.users.${username} = {
-          #     name = username;
-          #     home = "/Users/${username}";
-          #   };
-          #   home-manager.useGlobalPkgs = true;
-          #   home-manager.useUserPackages = true;
-          #   home-manager.users.${username} = import ./home.nix;
-          # }
         ];
       };
     };
