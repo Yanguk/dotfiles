1cc8db123a8dca6409e2080ee3cd3fa91f2c83b5
Yanguk <yanguk@duck.com>
2025-03-19 08:00:14 +0900

    blink C-b 활성화

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -28,16 +28,18 @@ return {
       ["<Up>"] = {},
       ["<Down>"] = {},
       -- ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+      -- ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      -- ['<Up>'] = { 'select_prev', 'fallback' },
+      -- ['<Down>'] = { 'select_next', 'fallback' },
 
+      -- ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      -- ["<C-f>"] = { "scroll_documentation_down", "fallback" },
       -- ["<C-e>"] = { "hide" },
       -- ["<C-y>"] = { "select_and_accept" },
       -- ["<C-p>"] = { "select_prev", "fallback" },
       -- ["<C-n>"] = { "select_next", "fallback" },
       -- ["<Tab>"] = { "snippet_forward", "fallback" },
       -- ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-      ["<C-b>"] = {},
-      ["<C-f>"] = {},
     },
     snippets = {
       preset = "luasnip",
