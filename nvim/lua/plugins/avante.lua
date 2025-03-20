3901e72bcf0e7869f9edc6971bb97bbe011907c8
Yanguk <yanguk@duck.com>
2025-03-21 08:08:28 +0900

    avante 모델 변경

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -3,7 +3,14 @@ return {
   event = "VeryLazy",
   opts = {
     provider = "copilot",
-    -- hints = { enabled = false },
+    copilot = {
+      model = "claude-3.7-sonnet",
+      proxy = nil, -- [protocol://]host[:port] Use this proxy
+      allow_insecure = false, -- Allow insecure server connections
+      timeout = 30000, -- Timeout in milliseconds
+      temperature = 0,
+      max_tokens = 8192,
+    },
   },
   build = "make",
   dependencies = {
