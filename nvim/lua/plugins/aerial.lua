d37798d18c256718d21a55cb0e1f40e4c2a26dc4
Yanguk <yanguk@duck.com>
2025-03-22 10:27:07 +0900

    불필요한 옵션 삭제

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -1,12 +1,6 @@
 return {
   "stevearc/aerial.nvim",
-  opts = {
-    on_attach = function(bufnr)
-      -- Jump forwards/backwards with '{' and '}'
-      vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-      vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-    end,
-  },
+  opts = {},
   keys = {
     {
       "<leader>ae",
