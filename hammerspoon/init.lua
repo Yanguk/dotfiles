88bf03978f99afa6cf22f1a21512bbd3b22ed978
Yanguk <yanguk@duck.com>
2025-04-03 08:13:45 +0900

    한영전한전에 키스트로크 추가

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,14 +2,31 @@ hs.loadSpoon("EmmyLua")
 
 local inputEnglishColemakDh = "io.github.colemakmods.keyboardlayout.colemakdh.colemakdhansi"
 
-local function changeInputSourceKrToEnWhenEscapeVim()
+-- 입력 소스를 영어로 변경하는 공통 함수
+local function changeInputSourceToEnglish()
   local inputSource = hs.keycodes.currentSourceID()
 
   if not (inputSource == inputEnglishColemakDh) then
+    hs.eventtap.keyStroke({}, "right")
     hs.keycodes.currentSourceID(inputEnglishColemakDh)
   end
+end
 
+local function changeInputSourceKrToEnWhenEscapeVim()
+  changeInputSourceToEnglish()
   hs.eventtap.keyStroke({}, "escape")
 end
 
 hs.hotkey.bind({ "control" }, 33, changeInputSourceKrToEnWhenEscapeVim)
+
+local esc_bind
+
+local function convert_to_eng_with_esc()
+  changeInputSourceToEnglish()
+
+  esc_bind:disable()
+  hs.eventtap.keyStroke({}, "escape")
+  esc_bind:enable()
+end
+
+esc_bind = hs.hotkey.new({}, "escape", convert_to_eng_with_esc):enable()
