071416abbe30c3344b7d4e2ad82a36320d5d3900
Yanguk <yanguk@duck.com>
2025-04-09 13:40:38 +0900

    bridge도 format 적용

--- nvim/lua/configs/by-domain.lua
+++ nvim/lua/configs/by-domain.lua
@@ -3,7 +3,7 @@ local M = {}
 M.isDisableAutoFormatBuf = function(bufnr)
   local bufname = vim.api.nvim_buf_get_name(bufnr)
 
-  local patterns = { "bridge", "tooling", "server%-common" }
+  local patterns = { "tooling", "server%-common" }
 
   for _, pattern in ipairs(patterns) do
     if bufname:find(pattern) then
