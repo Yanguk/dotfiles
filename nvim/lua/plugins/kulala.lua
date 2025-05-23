bdeaac9cfe54df390361e8ad58e6cb9f79268248
Yanguk <yanguk@duck.com>
2025-05-23 20:20:23 +0900

    kulala use default keymap
    Signed-off-by: Yanguk <yanguk@duck.com>

--- nvim/lua/plugins/kulala.lua
+++ nvim/lua/plugins/kulala.lua
@@ -1,1 +1,18 @@
-return { "mistweaverco/kulala.nvim", version = "*", opts = {}, ft = { "http" } }
+return {
+  "mistweaverco/kulala.nvim",
+  cmd = "KulalaScratchpad",
+  ft = { "http", "rest" },
+  opts = {
+    -- your configuration comes here
+    global_keymaps = true,
+    global_keymaps_prefix = "<leader>R",
+    kulala_keymaps_prefix = "",
+  },
+  config = function(_, opts)
+    require("kulala").setup(opts)
+
+    vim.api.nvim_create_user_command("KulalaScratchpad", function()
+      require("kulala").scratchpad()
+    end, { desc = "Open Kulala Scratchpad" })
+  end,
+}
