30c45473c5f6c3e10b02b8f4c8c9e49affbbbc5b
Yanguk <yanguk@duck.com>
2025-05-28 19:57:07 +0900

    feat: update autopairs
    Signed-off-by: Yanguk <yanguk@duck.com>

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,15 +1,7 @@
 return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
-  opts = {},
-  config = function()
-    local npairs = require("nvim-autopairs")
-    npairs.setup()
-
-    local Rule = require("nvim-autopairs.rule")
-    local cond = require("nvim-autopairs.conds")
-
-    npairs.remove_rule("[")
-    npairs.add_rule(Rule("[", "]"):with_pair(cond.not_filetypes({ "markdown" })))
-  end,
+  opts = {
+    disable_filetype = { "markdown" },
+  },
 }
