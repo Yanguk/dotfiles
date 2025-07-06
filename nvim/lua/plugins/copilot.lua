9dd37d46011e477557abbd35ad58eb1b3656237c
Yanguk <yanguk@duck.com>
2025-07-06 12:53:57 +0900

    add mdx
    Signed-off-by: Yanguk <yanguk@duck.com>

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -8,6 +8,7 @@ return {
     filetypes = {
       markdown = false,
       octo = false,
+      mdx = false,
       sh = function()
         if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
           -- disable for .env files
