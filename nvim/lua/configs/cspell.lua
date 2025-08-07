4d5d2cb5a06b77ed98e38e12f4fea4b462882940
Yanguk <yanguk@duck.com>
2025-08-07 22:22:55 +0900

    update: cspell
    Signed-off-by: Yanguk <yanguk@duck.com>

--- nvim/lua/configs/cspell.lua
+++ nvim/lua/configs/cspell.lua
@@ -8,13 +8,36 @@ function M.get_dictionary_path()
   return vim.fn.expand("$HOME/.config/cspell/my-dictionary.txt")
 end
 
--- Add unknown word under cursor to dictionary
 function M.add_word_to_c_spell_dictionary()
-  local word = vim.fn.expand("<cword>")
+  -- Get diagnostics source and only get from cspell
+  local bufnr = vim.api.nvim_get_current_buf()
+  local winnr = vim.api.nvim_get_current_win()
+  local cursor = vim.api.nvim_win_get_cursor(winnr)
+
+  local diagnostics = vim.diagnostic.get(bufnr, { lnum = cursor[1] - 1 })
+  local cspell_diagnostics = {}
+  for _, diagnostic in ipairs(diagnostics) do
+    if diagnostic.source == "cspell" then
+      table.insert(cspell_diagnostics, diagnostic)
+    end
+  end
+
+  -- Check if there are any cspell diagnostics
+  if #cspell_diagnostics == 0 then
+    vim.notify("No cSpell diagnostics found on current line", vim.log.levels.WARN, { title = "cSpell" })
+    return
+  end
+
+  -- Get the first word from the first cspell diagnostic
+  -- E.g. "Unknown word ( word )"
+  local word = cspell_diagnostics[1].message:match("%((.+)%)")
+  if word == nil then
+    vim.notify("Could not find unknown word", vim.log.levels.WARN, { title = "cSpell" })
+    return
+  end
 
   -- Show popup to confirm the action
   local confirm = vim.fn.confirm("Add '" .. word .. "' to cSpell dictionary?", "&Yes\n&No", 2)
-
   if confirm ~= 1 then
     return
   end
--- nvim/lua/plugins/mason-tool-installer.lua
+++ nvim/lua/plugins/mason-tool-installer.lua
@@ -38,6 +38,7 @@ return {
       "yaml-language-server",
       "yamlfmt",
       "beautysh",
+      "cspell",
     },
   },
 }
