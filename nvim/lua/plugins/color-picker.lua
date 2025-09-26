04db88ae8cb4c41908894b8db487022a8d2f7282
Yanguk <yanguk@duck.com>
2025-09-26 22:12:13 +0900

    add color picker
    Signed-off-by: Yanguk <yanguk@duck.com>

---
+++ nvim/lua/plugins/color-picker.lua
@@ -0,0 +1,31 @@
+return {
+  "eero-lehtinen/oklch-color-picker.nvim",
+  event = "VeryLazy",
+  version = "*",
+  keys = {
+    -- One handed keymap recommended, you will be using the mouse
+    {
+      "<leader>cv",
+      function()
+        require("oklch-color-picker").pick_under_cursor()
+      end,
+      desc = "Open Color pick under cursor",
+    },
+    {
+      "<leader>co",
+      function()
+        require("oklch-color-picker").open_picker()
+      end,
+      desc = "Open Color picker",
+    },
+    {
+      "<leader>ch",
+      function()
+        require("oklch-color-picker").highlight.toggle()
+      end,
+      desc = "Toggle highlight",
+    },
+  },
+  ---@type oklch.Opts
+  opts = {},
+}
