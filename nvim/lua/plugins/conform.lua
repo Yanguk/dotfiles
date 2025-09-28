a903fe3b8149965d69e25802d8e70ae454e6d02c
Yanguk <yanguk@duck.com>
2025-09-28 22:33:19 +0900

    add mdx conform
    Signed-off-by: Yanguk <yanguk@duck.com>

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -61,6 +61,7 @@ return {
       javascriptreact = jsTsFormatter,
       typescriptreact = jsTsFormatter,
       markdown = { "prettierd" },
+      mdx = { "prettierd" },
       html = { "prettierd" },
       json = { "prettierd" },
       jsonc = { "prettierd" },
