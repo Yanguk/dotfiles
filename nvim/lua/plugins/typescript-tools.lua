bd7cfc4c6713fa04a3df5c987c88a29473c16ae7
Yanguk <yanguk@duck.com>
2025-10-04 22:12:06 +0900

    feat: typescript-tools lazy
    Signed-off-by: Yanguk <yanguk@duck.com>

--- nvim/lua/plugins/typescript-tools.lua
+++ nvim/lua/plugins/typescript-tools.lua
@@ -3,6 +3,7 @@ local default_config = require("configs.lspconfig")
 return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
+  ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
   opts = {
     on_init = default_config.on_init,
     on_attach = default_config.on_attach,
